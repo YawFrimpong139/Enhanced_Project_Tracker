512fb1aa4ef7f7d47cf20342fd79213ac7d4ee62
YawFrimpong139 <yawzealow@gmail.com>
2025-06-15 14:54:30 +0100

    chore: Initial commit for project setup

---
+++ src/main/java/com/codewithzea/myprojecttracker/MyProjectTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.codewithzea.myprojecttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithzea/myprojecttracker/MyProjectTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithzea.myprojecttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
