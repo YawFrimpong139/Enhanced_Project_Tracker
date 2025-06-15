55716e0447504fe7d12107cc4d9cd679afe5a492
YawFrimpong139 <yawzealow@gmail.com>
2025-06-15 16:01:46 +0100

    chore: Add Audit details
    	new file:   src/main/java/com/codewithzea/myprojecttracker/audit/AuditLog.java
    	new file:   src/main/java/com/codewithzea/myprojecttracker/audit/AuditLogController.java
    	new file:   src/main/java/com/codewithzea/myprojecttracker/audit/AuditLogDTO.java
    	new file:   src/main/java/com/codewithzea/myprojecttracker/audit/AuditLogRepository.java
    	new file:   src/main/java/com/codewithzea/myprojecttracker/audit/AuditLogService.java
    	new file:   src/main/java/com/codewithzea/myprojecttracker/config/OpenApiConfig.java
    	new file:   src/main/java/com/codewithzea/myprojecttracker/config/RedisConfig.java

---
+++ src/main/java/com/codewithzea/myprojecttracker/audit/AuditLogController.java
@@ -0,0 +1,25 @@
+package com.codewithzea.myprojecttracker.audit;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/logs")
+@RequiredArgsConstructor
+public class AuditLogController {
+
+    private final AuditLogService auditLogService;
+
+    @GetMapping
+    public ResponseEntity<List<AuditLogDTO>> getLogs(@RequestParam(required = false) String entityType,
+                                                     @RequestParam(required = false) String actorName) {
+        return ResponseEntity.ok(auditLogService.getLogs(entityType, actorName));
+    }
+}
+
+
+
---
+++ src/main/java/com/codewithzea/myprojecttracker/audit/AuditLogDTO.java
@@ -0,0 +1,23 @@
+package com.codewithzea.myprojecttracker.audit;
+
+
+import lombok.*;
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuditLogDTO {
+    private String id;
+    private String actionType;
+    private String entityType;
+    private String entityId;
+    private Instant timestamp;
+    private String actorName;
+    private String payload;
+}
+
+
+
